484063454037571c43e503f79e464be2a97a9a5d
thauu-01 <popola957th@gmail.com>
2025-09-26 09:57:54 +0700

    Update ProductRepository.java

--- baitap09/src/main/java/baitap09/repository/ProductRepository.java
+++ baitap09/src/main/java/baitap09/repository/ProductRepository.java
@@ -2,10 +2,14 @@ package baitap09.repository;
 
 import baitap09.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     List<ProductEntity> findAllByOrderByPriceAsc();
-    List<ProductEntity> findByCategoriesId(Long categoryId);
+
+ 
+    List<ProductEntity> findByCategories_Id(Long categoryId);
 }
